6925545b292b993400883d2069c93ca619b0be03
Martin Reinok <martin.reinok@gmail.com>
2022-08-21 14:42:36 +0300

    Math counter app

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,14 +1,10 @@
-import React, {useState} from 'react';
+import React from 'react';
 import logo from './logo.svg';
-import './App.css';
-import Clock from './components/Clock'
+import Counter from './components/counter';
+
 
 function App() {
-  return  (
-    <div className='App'>
-      <Clock></Clock>
-    </div>
-  );
+  return  <Counter />;
 }
 
 export default App;
