97f9b0fe7b6ff873bd8d889aacb168a6f560f303
Martin Reinok <martin.reinok@gmail.com>
2022-09-04 22:40:39 +0200

    Updated project structure

--- javascript-app/modules/flow-field.js
+++ javascript-app/modules/flow-field.js
@@ -1,13 +1,13 @@
 
 var points = []
-var angleMultiplier = 1
+var angleMultiplier = 0.001
 
 function setup() {
     let renderer = createCanvas(windowWidth, windowHeight);
     renderer.parent("flow-field-canvas");
     background(0);
     
-    var density = 80
+    var density = 40
 
     var space = width / density
 
@@ -24,13 +24,13 @@ function draw() {
 
     for (var i = 0; i < points.length; i++) {
 
-        var r = map(points[i].x, 0, width, 255, 50)
-        var g = map(points[i].x, 0, width, 255, 50)
-        var b = map(points[i].x, 0, width, 50, 255)
+        var r = map(points[i].x, 0, width, 200, 100)
+        var g = map(points[i].x, 0, width, 0, 255)
+        var b = map(points[i].x, 0, width, 0, 255)
 
         fill(r, g, b)
 
-        var angle = map(noise(points[i].x * angleMultiplier, points[i].y * angleMultiplier), 0, 1, 0, 720)
+        var angle = map(noise(points[i].x * angleMultiplier, points[i].y * angleMultiplier), 0, 10, 100, 100000)
         points[i].add(createVector(cos(angle), sin(angle)))
         ellipse(points[i].x, points[i].y, 2)
     }
