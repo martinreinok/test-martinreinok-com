4af1886f16e60184393a2425f528523aaddf3f20
Martin Reinok <martin.reinok@gmail.com>
2022-09-04 22:54:25 +0200

    Updated pipeline

--- javascript-app/modules/clock.js
+++ javascript-app/modules/clock.js
@@ -1,13 +1,13 @@
 
 export function clockApp() {
-    setInterval(setClock, 1)
+    setInterval(updateClock, 1)
 
     const hourHand = document.querySelector('[data-hour-hand]')
     const minuteHand = document.querySelector('[data-minute-hand]')
     const secondHand = document.querySelector('[data-second-hand]')
     const millisecondHand = document.querySelector('[data-millisecond-hand]')
 
-    function setClock() {
+    function updateClock() {
     const currentDateClock = new Date()
     const millisecondsRatio = currentDateClock.getMilliseconds() / 1000
     const secondsRatio = currentDateClock.getSeconds() / 60
@@ -17,13 +17,12 @@ export function clockApp() {
     setRotation(secondHand, secondsRatio)
     setRotation(minuteHand, minutesRatio)
     setRotation(hourHand, hoursRatio)
-    //document.getElementById("clock-time").innerHTML = currentDateClock.getHours() + ":" + currentDateClock.getMinutes() + ":" + currentDateClock.getSeconds()
     }
 
     function setRotation(element, rotationRatio) {
     element.style.setProperty('--rotation', rotationRatio * 360)
     }
-    
-    setClock()
+
+    updateClock()
   }
 
